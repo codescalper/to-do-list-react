64e8fbc31c5bae9f400aa42805f50426a1247253
codescalper <msfunbook@gmail.com>
2023-02-22 17:09:02 +0530

    On clicking remove all it will empty the items

--- to-do-list/src/components/Todo.js
+++ to-do-list/src/components/Todo.js
@@ -4,17 +4,35 @@ import './style.css'
 
 export const Todo  = () => {
 
-    const [inputdata , setInputData] = useState("");
+    const [inputData , setInputData] = useState("");
     const [item , setItem] = useState([]);
     
     function addItem(){
-        if(!inputdata){
+        if(!inputData){
             alert('Please fill the data');
         }else{
-            setItem([...item, inputdata]);
+
+            const myNewInputData = {
+                id: new Date().getTime().toString(),
+                name:inputData,
+            }
+            setItem([...item, myNewInputData]);
 
             setInputData("  ")
         }
+    }
+
+    function deleteItem(index) {
+        const updatedItems = item.filter((currElem)=>{
+            return currElem.id!==index;
+
+        })
+        setItem(updatedItems);
+    }   
+
+
+    function removeAll(){
+        setItem([]);
     }
 
   return (
@@ -26,17 +44,17 @@ export const Todo  = () => {
             <figcaption>Create your to-do list here</figcaption>
         </figure>
         <div className='addItems'>
-            <input type = "text" placeholder = "Add Items" className='form-ctrl' value={inputdata} onChange={(e)=> setInputData(e.target.value)} />
-            <i class="fa fa-solid fa-plus add-btn" onClick={addItem} ></i>
+            <input type = "text" placeholder = "Add Items" className='form-ctrl' value={inputData} onChange={(e)=> setInputData(e.target.value)} />
+            <i className="fa fa-solid fa-plus add-btn" onClick={addItem}></i>
         </div>
         <div className='showItems'>
         {item.map((currElem,index)=>{
             return(
-                  <div className='eachItem' key={index}>
-                  <h3>{currElem}  </h3>
+                  <div className='eachItem' key={currElem.id}>
+                  <h3>{currElem.name}  </h3>
                   <div className='todo-btn'>
-                  <i class="fa fa-solid fa-edit add-btn" ></i>
-                  <i class="fa fa-solid fa-trash add-btn" ></i>
+                  <i className="far fa-solid fa-edit add-btn" ></i>
+                  <i className="far fa-solid fa-trash-alt add-btn" onClick={()=>deleteItem(currElem.id)} ></i>
                   </div>  
                   </div>
                       )
@@ -45,7 +63,7 @@ export const Todo  = () => {
         </div>
 
          <div className='showItems'>
-          <button className='btn effect' data-sm-link-text="Remove All" ><span>Check List</span></button>
+          <button className='btn effect' data-sm-link-text="Remove All" onClick={removeAll} ><span>Check List</span></button>
         </div>
         </div>      
     </div>
